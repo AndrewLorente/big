f1f5151c452db433d63c0e38c13a8e177947de78
Andrew Lorente <hello@andrewlorente.com>
2015-04-05 13:07:01 -0700

    Style changes
    one- and two-letter variables now have meaningful names.
    all if statements have braces.
    Switched a multi `=== || ===` to `~ [].indexOf`.
    Lots of newlines.
    jshint.

--- big.js
+++ big.js
@@ -1,60 +1,115 @@
 window.onload = function() {
-    var s = document.getElementsByTagName('div'), ti;
-    if (!s) return;
-    var big = { current: 0, forward: fwd, reverse: rev, go: go, length: s.length };
-    window.big = big;
-    function resize() {
-        var w = window.innerWidth, h = window.innerHeight, e = s[big.current];
-        e.style.fontSize = h + 'px';
-        for (var i = h - 2; e.offsetWidth > w || e.offsetHeight > h; i -= 2) {
-            e.style.fontSize = i + 'px';
-        }
-        e.style.marginTop = ((h - e.offsetHeight) / 2) + 'px';
+    'use strict';
+    var slides = document.getElementsByTagName('div'),
+        timerInterval;
+    if (!slides) {
+        return;
     }
-    function go(n) {
-        big.current = n;
-        var e = s[n], t = parseInt(e.dataset.timeToNext || 0, 10);
-        document.body.className = e.dataset.bodyclass || '';
-        for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
-        e.style.display = 'inline';
-        if (e.firstChild && e.firstChild.nodeName === 'IMG') {
-            document.body.style.backgroundImage = 'url("' + e.firstChild.src + '")';
-            e.firstChild.style.display = 'none';
-            if ('classList' in e) e.classList.add('imageText');
+    var big = {
+        current: 0,
+        forward: fwd,
+        reverse: rev,
+        go: go,
+        length: slides.length
+    };
+    window.big = big;
+
+    function resize() {
+        var width = window.innerWidth,
+            height = window.innerHeight,
+            element = slides[big.current];
+        // start the slide's text at "as tall as the window"
+        element.style.fontSize = height + 'px';
+        // now reduce it by 2 pixel-steps until it's small enough to fit
+        for (var i = height - 2; element.offsetWidth > width || element.offsetHeight > height; i -= 2) {
+            element.style.fontSize = i + 'px';
+        }
+        // vertically center the text by setting a margin-top equal to half the available empty space
+        element.style.marginTop = ((height - element.offsetHeight) / 2) + 'px';
+    }
+
+    function go(pageNum) {
+        big.current = pageNum;
+        var slide = slides[pageNum],
+            timeToNext = parseInt(slide.dataset.timeToNext || 0, 10);
+        document.body.className = slide.dataset.bodyclass || '';
+        slides.forEach(function(slide) {
+            slide.style.display = 'none';
+        });
+        slide.style.display = 'inline';
+
+        if (slide.firstChild && slide.firstChild.nodeName === 'IMG') {
+            document.body.style.backgroundImage = 'url("' + slide.firstChild.src + '")';
+            slide.firstChild.style.display = 'none';
+            if ('classList' in slide) {
+                slide.classList.add('imageText');
+            }
         } else {
             document.body.style.backgroundImage = '';
-            document.body.style.backgroundColor = e.style.backgroundColor;
+            document.body.style.backgroundColor = slide.style.backgroundColor;
         }
-        if (ti !== undefined) window.clearInterval(ti);
-        if (t > 0) ti = window.setTimeout(fwd, (t * 1000));
+        if (timerInterval !== undefined) {
+            window.clearInterval(timerInterval);
+        }
+        if (timeToNext > 0){
+            timerInterval = window.setTimeout(fwd, (timeToNext * 1000));
+        }
         resize();
-        if (window.location.hash !== n) window.location.hash = n;
-        document.title = e.textContent || e.innerText;
+        if (window.location.hash !== pageNum) {
+            window.location.hash = pageNum;
+        }
+        document.title = slide.textContent || slide.innerText;
     }
-    document.onclick = function() { go(++big.current % (s.length)); };
-    function fwd() { go(Math.min(s.length - 1, ++big.current)); }
-    function rev() { go(Math.max(0, --big.current)); }
-    document.onkeydown = function(e) {
-        if (e.which === 39 || e.which === 34 || e.which === 40) fwd();
-        if (e.which === 37 || e.which === 33 || e.which === 38) rev();
+
+    document.onclick = function() {
+        go(++big.current % (slides.length));
     };
-    document.ontouchstart = function(e) {
-        var x0 = e.changedTouches[0].pageX;
-        document.ontouchend = function(e2) {
-            var x1 = e2.changedTouches[0].pageX;
-            if (x1 - x0 < 0) fwd();
-            if (x1 - x0 > 0) rev();
+
+    function fwd() {
+        go(Math.min(slides.length - 1, ++big.current));
+    }
+    function rev() {
+        go(Math.max(0, --big.current));
+    }
+
+    document.onkeydown = function(event) {
+        if (~ [39, 34, 40].indexOf(event.which)) {
+            fwd();
+        }
+        if (~ [37, 33, 38].indexOf(event.which)) {
+            rev();
+        }
+    };
+
+    document.ontouchstart = function(startEvent) {
+        var start = startEvent.changedTouches[0].pageX;
+        document.ontouchend = function(endEvent) {
+            var end = endEvent.changedTouches[0].pageX;
+            if (end - start < 0) fwd();
+            if (end - start > 0) rev();
         };
     };
+
     function parse_hash() {
-        return Math.max(Math.min(s.length - 1,
-            parseInt(window.location.hash.substring(1), 10)), 0);
+        return Math.max(
+            Math.min(
+                slides.length - 1,
+                parseInt(window.location.hash.substring(1), 10)
+            ),
+        0);
     }
-    if (window.location.hash) big.current = parse_hash() || big.current;
+
+    if (window.location.hash) {
+        big.current = parse_hash() || big.current;
+    }
+
     window.onhashchange = function() {
-        var c = parse_hash();
-        if (c !== big.current) go(c);
+        var pageNum = parse_hash();
+        if (pageNum !== big.current) {
+            go(pageNum);
+        }
     };
+
     window.onresize = resize;
     go(big.current);
 };
